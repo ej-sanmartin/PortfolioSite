1a86c8d164bfd3d7db217c6cfe8be5da42f8577a
Edgar Jr. San Martin <esanmartinjr@fordham.edu>
2018-07-25 15:27:18 -0400

    Update Footer.js

--- client/src/routes/Footer.js
+++ client/src/routes/Footer.js
@@ -1,29 +1,33 @@
 import React, { Component } from "react";
 import { SocialIcon } from 'react-social-icons';
-import { Nav, NavItem, Row, Col } from 'reactstrap';
+import { Nav, Row, Col } from 'reactstrap';
 
 export default class Footer extends Component {
   render() {
     const footer = {
-      bottom: "0",
-      width: "100%",
+      width: "103.5%",
       backgroundColor: "#BF84D4",
+      position: "relative",
+      right: "0",
+      bottom: "0",
+      left: "0",
+      padding: "2rem",
+      borderTop: "solid .01em #61099b",
+      zIndex: "1"
+    }
+
+    const icons = {
+      marginTop: "1em",
+      marginRight: "1em"
     }
 
     return (
       <Row style={footer}>
-        <Col sm={{ size: 2, offset: 4 }}>
-          <SocialIcon url="https://github.com/ej-sanmartin" color="#F5E3FB" />
-        </Col>
-        <Col sm={{ size: 2, offset: 6 }}>
-          <SocialIcon url="https://angel.co/edgar-jr-san-martin" color="#F5E3FB" />
-        </Col>
-        <Col sm={{ size: 2, offset: 8 }}>>
-          <SocialIcon url="https://www.linkedin.com/in/edgar-jr-san-martin/" color="#F5E3FB" />
-        </Col>
-        <Col sm={{ size: 2, offset: 10 }}>
-          <SocialIcon url="https://www.instagram.com/ej_sanmartin/" color="#F5E3FB" />
-        </Col>
+          <SocialIcon style={icons} url="https://github.com/ej-sanmartin" color="#F5E3FB" />
+          <SocialIcon style={icons} url="https://www.linkedin.com/in/edgar-jr-san-martin/" color="#F5E3FB" />
+          <SocialIcon style={icons} url="https://www.youtube.com/channel/UC_JqyD1Kseb0-e8Q9HWRMdQ" color="#F5E3FB" />
+          <SocialIcon style={icons} url="https://angel.co/edgar-jr-san-martin" color="#F5E3FB" />
+          <SocialIcon style={icons} url="https://www.instagram.com/ej_sanmartin/" color="#F5E3FB" />
       </Row>
     );
   }
